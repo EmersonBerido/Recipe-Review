c9a6690b12f654a9433feaa04f968ae61ee00a85
EmersonBerido <beridoemerson05@gmail.com>
2025-09-26 10:46:10 -0700

    Base Login done

--- src/Components/CreatePost.tsx
+++ src/Components/CreatePost.tsx
@@ -49,7 +49,7 @@ function CreatePost() {
     //probably cache user data so we dont have to fetch EVERY single time
     //all posts should be in a shared cache so other browsers dont have to independently fetch them if theres multiple ppl using it
 
-    const date : Post = {
+    const data : Post = {
       food : event.target.food.value,
       recipe : event.target.recipe.value,
       image : "PLACEHOLDER; REPLACE SOON!",
@@ -73,10 +73,14 @@ function CreatePost() {
 
 
     }
+
+    //Store post in database
+    console.log(data);
   }
+  
 
   return (
-    <main>
+    <main style={{border : "3px solid black"}}>
       <h1>Create Post</h1>
       <form onSubmit={handlePostSubmit}>
         <label htmlFor="food">Food item</label>
@@ -95,69 +99,70 @@ function CreatePost() {
           <label htmlFor="execution">Your execution</label>
           <input type="number" name="execution" min={1} max={5}/>
 
-          <textarea name="description" placeholder="Today I baked a..."/>
-
-          <div className="date-form-container">
-            <select name="day" defaultValue={new Date().getDate()}>
-              <option value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-              <option value="4">4</option>
-              <option value="5">5</option>
-              <option value="6">6</option>
-              <option value="7">7</option>
-              <option value="8">8</option>
-              <option value="9">9</option>
-              <option value="10">10</option>
-              <option value="11">11</option>
-              <option value="12">12</option>
-              <option value="13">13</option>
-              <option value="14">14</option>
-              <option value="15">15</option>
-              <option value="16">16</option>
-              <option value="17">17</option>
-              <option value="18">18</option>
-              <option value="19">19</option>
-              <option value="20">20</option>
-              <option value="21">21</option>
-              <option value="22">22</option>
-              <option value="23">23</option>
-              <option value="24">24</option>
-              <option value="25">25</option>
-              <option value="26">26</option>
-              <option value="27">27</option>
-              <option value="28">28</option>
-              <option value="29">29</option>
-              <option value="30">30</option>
-              <option value="31">31</option>   
-            </select>
-
-            <select name="month" defaultValue={new Date().getMonth() + 1}>
-              <option value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-              <option value="4">4</option>
-              <option value="5">5</option>
-              <option value="6">6</option>
-              <option value="7">7</option>
-              <option value="8">8</option>
-              <option value="9">9</option>
-              <option value="10">10</option>
-              <option value="11">11</option>
-              <option value="12">12</option>
-            </select>
-
-            <select name="year" defaultValue={new Date().getFullYear()}>
-              <option value="2021">2021</option>
-              <option value="2022">2022</option>
-              <option value="2023">2023</option>
-              <option value="2024">2024</option>
-              <option value="2025">2025</option>
-            </select>
-
-          </div>
-
         </div>
+
+        <textarea name="description" placeholder="Today I baked a..."/>
+
+        <div className="date-form-container">
+          <select name="day" defaultValue={new Date().getDate()}>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+            <option value="7">7</option>
+            <option value="8">8</option>
+            <option value="9">9</option>
+            <option value="10">10</option>
+            <option value="11">11</option>
+            <option value="12">12</option>
+            <option value="13">13</option>
+            <option value="14">14</option>
+            <option value="15">15</option>
+            <option value="16">16</option>
+            <option value="17">17</option>
+            <option value="18">18</option>
+            <option value="19">19</option>
+            <option value="20">20</option>
+            <option value="21">21</option>
+            <option value="22">22</option>
+            <option value="23">23</option>
+            <option value="24">24</option>
+            <option value="25">25</option>
+            <option value="26">26</option>
+            <option value="27">27</option>
+            <option value="28">28</option>
+            <option value="29">29</option>
+            <option value="30">30</option>
+            <option value="31">31</option>   
+          </select>
+
+          <select name="month" defaultValue={new Date().getMonth() + 1}>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+            <option value="7">7</option>
+            <option value="8">8</option>
+            <option value="9">9</option>
+            <option value="10">10</option>
+            <option value="11">11</option>
+            <option value="12">12</option>
+          </select>
+
+          <select name="year" defaultValue={new Date().getFullYear()}>
+            <option value="2021">2021</option>
+            <option value="2022">2022</option>
+            <option value="2023">2023</option>
+            <option value="2024">2024</option>
+            <option value="2025">2025</option>
+          </select>
+        </div>
+
+        <button type="submit">Post</button>
 
       </form>
     </main>
--- src/Routes/Login.tsx
+++ src/Routes/Login.tsx
@@ -1,13 +1,80 @@
 import {useNavigate} from "react-router-dom";
+import {useState} from "react";
+
+//Important Notes:
+// 1. Logging in will send you to /home
+// 2. Will use oAuth in the future for authentication
+// 3. For now, it will store in local storage
+// 4. When logging in, username and pfp will be saved in pfp
+// 5. Maybr add a forget password
 
 
 function Login() {
-  const Navigate = useNavigate();
+  const navigate = useNavigate();
+  console.log("hi")
+
+  const [existingUser, setExistingUser] = useState(true);
+
+  function handleLogin(event : any) 
+  {
+    event.preventDefault();
+    console.log("Logging in");
+
+    //user will be stored with name user, and values name and pfp
+    // for now, password will also be stored in local storage
+
+    const data = localStorage.getItem("user")
+
+    if (existingUser)
+    {
+      //check if user exists in database
+      //local storage for now
+      if (data === null)
+      {
+        alert("User doesn't exist");
+      }
+      else if (JSON.parse(data).password === event.target.password.value)
+      {
+        console.log("User logged in");
+        navigate("/home");
+      }
+      else
+      {
+        alert("wrong password");
+      }
+    }
+    else
+    {
+      //add user to database; local storage
+      localStorage.setItem("user",JSON.stringify({
+        username : event.target.username.value, 
+        password : event.target.password.value,
+        picture : "PLACEHOLDER; REPLACE SOON!"
+      }));
+      console.log("User created");
+      navigate("/home");
+    }
+  }
 
   return (
     <main>
       <h1>Login</h1>
-      <button onClick={() => Navigate("/home")}>Home</button>
+      <form onSubmit={handleLogin}>
+        <input type="text" name="username" placeholder="Username" required/>
+        <input type="password" name="password" placeholder="Password" required/>
+        <button type="submit">{existingUser ? "Log in" : "Sign Up"}</button>
+        {existingUser 
+        ? 
+          <button type="button" onClick={() => setExistingUser(false)}>
+            New User?
+          </button>
+        : 
+          <button type="button" onClick={() => setExistingUser(true)}>
+            Have an account?
+          </button>
+        }
+  
+      </form>
     </main>
   )
 }
